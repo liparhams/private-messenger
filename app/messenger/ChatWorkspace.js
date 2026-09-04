c0798a22ae9e70702278ccb8f2b9642d2eb53f94
Parham Soleimany <linderrobin71@gmail.com>
2026-09-04 06:58:59 +0330

    style: improve messenger creation and chat surfaces

--- app/messenger/ChatWorkspace.js
+++ app/messenger/ChatWorkspace.js
@@ -2,6 +2,7 @@
 import "./v1.css";
 import "./platform-polish.css";
 import "../telegram-ui.css";
+import "./creation-polish.css";
 import ProfilePanel from "./ProfilePanel";
 import ChatWorkspaceImpl from "./ChatWorkspaceImpl";
 export default function ChatWorkspace(){return <><ChatWorkspaceImpl/><ProfilePanel/></>}
