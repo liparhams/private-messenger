2524611535fd75603ea29544e9b1674363292ac5
Parham Soleimany <linderrobin71@gmail.com>
2026-09-04 07:53:09 +0330

    ui: load UTINO messenger platform polish v2

--- app/messenger/ChatWorkspace.js
+++ app/messenger/ChatWorkspace.js
@@ -3,6 +3,7 @@ import "./v1.css";
 import "./platform-polish.css";
 import "../telegram-ui.css";
 import "./creation-polish.css";
+import "./platform-v2.css";
 import ProfilePanel from "./ProfilePanel";
 import ChatWorkspaceImpl from "./ChatWorkspaceImpl";
 export default function ChatWorkspace(){return <><ChatWorkspaceImpl/><ProfilePanel/></>}
