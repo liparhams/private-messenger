2da6091fb5d3837f85987b960acd5c902e56e948
Parham Soleimany <linderrobin71@gmail.com>
2026-09-04 23:20:43 +0330

    feat: apply refined messenger interaction layer

--- app/messenger/ChatWorkspace.js
+++ app/messenger/ChatWorkspace.js
@@ -6,5 +6,6 @@ import "./platform-v8.css";
 import "./production-polish.css";
 import "./messenger-final.css";
 import "./telegram-clean.css";
+import "./telegram-plus.css";
 import ChatWorkspaceImpl from "./ChatWorkspaceImpl";
 export default function ChatWorkspace(){return <ChatWorkspaceImpl/>}
