074e3488cb4f87ec0abf3bb4a95e466f22bbb6d5
Parham Soleimany <linderrobin71@gmail.com>
2026-09-04 18:59:03 +0330

    style: enable messenger platform v3 layer

--- app/messenger/ChatWorkspace.js
+++ app/messenger/ChatWorkspace.js
@@ -4,6 +4,7 @@ import "./platform-polish.css";
 import "../telegram-ui.css";
 import "./creation-polish.css";
 import "./platform-v2.css";
+import "./platform-v3.css";
 import ProfilePanel from "./ProfilePanel";
 import ChatWorkspaceImpl from "./ChatWorkspaceImpl";
 export default function ChatWorkspace(){return <><ChatWorkspaceImpl/><ProfilePanel/></>}
