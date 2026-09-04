1dc7cbe79b8d73c4e05b4d2e7524a227b11b2a13
Parham Soleimany <linderrobin71@gmail.com>
2026-09-04 21:45:57 +0330

    ui: activate production messenger polish layer

--- app/messenger/ChatWorkspace.js
+++ app/messenger/ChatWorkspace.js
@@ -3,6 +3,7 @@ import "./v1.css";
 import "./platform-v7.css";
 import "./creation-polish.css";
 import "./platform-v8.css";
+import "./production-polish.css";
 import SupportLauncher from "./SupportLauncher";
 import ChatWorkspaceImpl from "./ChatWorkspaceImpl";
 export default function ChatWorkspace(){return <><ChatWorkspaceImpl/><SupportLauncher/></>}
