2a2ff7f641c43dd7a554b83097c2e4c4d25afc6a
Parham Soleimany <linderrobin71@gmail.com>
2026-09-04 21:01:13 +0330

    refactor: remove profile panel from messenger header flow

--- app/messenger/ChatWorkspace.js
+++ app/messenger/ChatWorkspace.js
@@ -8,7 +8,7 @@ import "./platform-v3.css";
 import "./platform-v4.css";
 import "./platform-v5.css";
 import "./platform-v6.css";
-import ProfilePanel from "./ProfilePanel";
+import "./platform-v7.css";
 import SupportLauncher from "./SupportLauncher";
 import ChatWorkspaceImpl from "./ChatWorkspaceImpl";
-export default function ChatWorkspace(){return <><ChatWorkspaceImpl/><ProfilePanel/><SupportLauncher/></>}
+export default function ChatWorkspace(){return <><ChatWorkspaceImpl/><SupportLauncher/></>}
