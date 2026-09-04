79b4b9c3c309b010405d05a2f8c4703f98d855e5
Parham Soleimany <linderrobin71@gmail.com>
2026-09-04 21:34:45 +0330

    refactor: consolidate messenger styling layers

--- app/messenger/ChatWorkspace.js
+++ app/messenger/ChatWorkspace.js
@@ -1,14 +1,8 @@
 "use client";
 import "./v1.css";
-import "./platform-polish.css";
-import "../telegram-ui.css";
-import "./creation-polish.css";
-import "./platform-v2.css";
-import "./platform-v3.css";
-import "./platform-v4.css";
-import "./platform-v5.css";
-import "./platform-v6.css";
 import "./platform-v7.css";
+import "./creation-polish.css";
+import "./platform-v8.css";
 import SupportLauncher from "./SupportLauncher";
 import ChatWorkspaceImpl from "./ChatWorkspaceImpl";
 export default function ChatWorkspace(){return <><ChatWorkspaceImpl/><SupportLauncher/></>}
